96c64f41762b8e3c3a8a21a3c119d821082d0e21
Michael Colombo <mcolombo@redhat.com>
2023-05-03 12:02:50 -0400

    improved input handling further and converted play_again into a function.

--- guess_the_number.py
+++ guess_the_number.py
@@ -17,29 +17,33 @@ def guessing_game():
     for guesses_taken in range(1, 7):
         print("\nTake a guess")
         guess=int(input())
-        if guess < 0 or guess > 20 or guess == '':
+        if guess < 0 or guess > 20 or guess == 'None' :
             print("Please enter a number greater than 1 and less than 20." )
-            guessing_game 
-        if guess < secret_number:
+            guessing_game() 
+        elif guess < secret_number:
             print("Your guess is too low.")
         elif guess > secret_number:
             print("your guess is too high" )
-        else:
+        elif guess == secret_number:
+            print("Good Job, " + name + "! you guessed my number in " + str(guesses_taken) + " guesses!")
             break
-
-    if guess == secret_number:
-        print("Good Job, " + name + "! you guessed my number in " + str(guesses_taken) + " guesses!")
-    else:
-        print("Nope. The number I was thinking of was " + str(secret_number))
+        else:
+            print("Nope. The number I was thinking of was " + str(secret_number))
+            break
 
 guessing_game()
 
-play_again = str(input("\nDo you want to play again (type yes or no): " ))
-if play_again.lower() == 'yes':
-    guessing_game()
-if play_again.lower() == 'no':
-    print("good bye, " + name + "! Hope you had fun")  
-    sys.exit(0)
-else:
-    print("Not a valid response. Good bye, " + name + "!" )
-    sys.exit(0)
+    # Ask player if they want to play again
+
+def play_again():
+    play=str(input("\nDo you want to play again (type yes or no): " ))
+    if play.lower() == 'yes':
+        guessing_game()
+    if play.lower() == 'no':
+        print("good bye, " + name + "! Hope you had fun")  
+        sys.exit(0)
+    else:
+        print("Not a valid response!" )
+        play_again()
+
+play_again()
