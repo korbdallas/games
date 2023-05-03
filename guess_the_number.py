5192ae8451d779aec5183a3607600944521cf247
Michael Colombo <mcolombo@redhat.com>
2023-05-03 16:31:56 -0400

    Added additional error handling

--- guess_the_number.py
+++ guess_the_number.py
@@ -10,13 +10,31 @@ name=input()
 
 secret_number=random.randint(1, 20)
 
+def intro():
+    print("\nWell, " + name + ", I am thinking of a number between 1 and 20")
+
+
 def guessing_game():
-    print("Well, " + name + ", I am thinking of a number between 1 and 20")
 
     # Ask player to guess in 6 tries
     for guesses_taken in range(1, 7):
         print("\nTake a guess")
-        guess=int(input())
+        guess=input()
+        try:  # checks if input is numeric
+             guess = int(guess) 
+        except:  
+             print("Please enter a number greater than 1 and less than 20." )
+             pass
+             guessing_game()
+        try:
+            guess = float(guess)  # Checks to see if guess is a float 
+        except:
+            print("Please enter a number greater than 1 and less than 20.")
+            pass
+            guessing_game()
+   
+        guess = int(guess)
+
         if guess < 0 or guess > 20 or guess == 'None' :
             print("Please enter a number greater than 1 and less than 20." )
             guessing_game() 
@@ -26,19 +44,18 @@ def guessing_game():
             print("your guess is too high" )
         elif guess == secret_number:
             print("Good Job, " + name + "! you guessed my number in " + str(guesses_taken) + " guesses!")
-            break
-        else:
-            print("Nope. The number I was thinking of was " + str(secret_number))
-            break
-
-guessing_game()
+            play_again()
+    else:
+        print("Nope. The number I was thinking of was " + str(secret_number))
+        
 
     # Ask player if they want to play again
 
 def play_again():
     play=str(input("\nDo you want to play again (type yes or no): " ))
     if play.lower() == 'yes':
-        guessing_game()
+       print("\nWell, " + name + ", I am thinking of a number between 1 and 20")
+       guessing_game()
     if play.lower() == 'no':
         print("good bye, " + name + "! Hope you had fun")  
         sys.exit(0)
@@ -46,4 +63,6 @@ def play_again():
         print("Not a valid response!" )
         play_again()
 
+intro()
+guessing_game()
 play_again()
